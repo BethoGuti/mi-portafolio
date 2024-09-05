79ac7b8abf5c8650c8e6d62c65da8a4050921a2c
AlbertoOro <bet68guti@gmail.com>
2024-09-05 00:51:54 -0600

    Correción de estilos en el menú para móvil

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -42,8 +42,8 @@ export const Nav = ({ focus }) => {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <img
-              className="h-16 w-auto"
-              src="/logo.svg"
+              className="h-10 w-auto"
+              src="/logo_1.svg"
               alt=""
             />
           </a>
@@ -95,13 +95,12 @@ export const Nav = ({ focus }) => {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-2 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
               <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-10 w-auto"
+                src="/logo_1.svg"
                 alt=""
               />
             </a>
@@ -123,7 +122,7 @@ export const Nav = ({ focus }) => {
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                   >
-                    {item.name}
+                   &lt; {item.name} /&gt;
                   </a>
                 ))}
               </div>
